21112bebfc15ccf18943ad6dfb1fd5ecd635022f
Mitchell Mudd <mitchellmudd@outlook.com>
2022-10-31 20:53:27 -0400

    Finished part 3 of tutorial. Set up a graphql server using GraphQL Yoga and surfaced the graphQL api types using Pothos.

--- graphql-server/src/index.ts
+++ graphql-server/src/index.ts
@@ -1,1 +1,14 @@
-console.log('Hey there! 👋');
+// 1
+import { createServer } from "@graphql-yoga/node";
+import { schema } from "./schema";
+// 2
+const port = Number(process.env.API_PORT) || 4000
+// 3
+const server = createServer({
+  port,
+  schema
+});
+// 4
+server.start().then(() => {
+  console.log(`🚀 GraphQL Server ready at http://localhost:${port}/graphql`);
+});
